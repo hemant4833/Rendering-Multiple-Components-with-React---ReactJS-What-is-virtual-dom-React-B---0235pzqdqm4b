941103262ad76c8f7565dbfcb587bd3f6663201a
hemant4833 <73534621+hemant4833@users.noreply.github.com>
2022-10-18 04:09:00 +0530

    my commit for project

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,10 @@ import '../styles/App.css';
 
 const App = () => {
   return (
-    <div id="main"></div>
+    <>
+     <h1 data-ns-test="project-name">TO-DO-LIST</h1>
+        <p data-ns-test="project-description">To do list have an form to add todo in a paragraph way hence it has button to add an todo and also have footer!</p>
+      </>  
   )
 }
 
